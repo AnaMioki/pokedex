b29f48018523e0bd5f663a31fd9663b3adb2a154
Ana Carolina Fiorini Mioki <ana.fiorinimioki@gmail.com>
2024-09-02 23:33:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const pokemonName = document.querySelector('.pokemon_name')
+const pokemonNumber = document.querySelector('.pokemon_number')
+const pokemonImage = document.querySelector('.pokemon_image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+    if (APIResponse.status === 200){
+        
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {  
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    
+        
+        input.value = '';
+    } else{
+        pokemonName.innerHTML = 'Not Found :c';
+        pokemonNumber.innerHTML = '';
+        pokemonImage.innerHTML = '';
+    }
+
+  
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+});
